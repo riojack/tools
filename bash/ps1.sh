325be9fb754c15233f8e1802f39e36f776321b20
AJ Whatling <riojack@users.noreply.github.com>
2018-11-04 15:44:12 -0600

    Show directory name if it's 5 chars or less

--- bash/ps1.sh
+++ bash/ps1.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 is_gitted_directory_tree() {
 	$(git rev-parse --show-toplevel &> /dev/null)
 	local git_toplevel_ret=$?
@@ -5,14 +7,20 @@ is_gitted_directory_tree() {
 	if [ $git_toplevel_ret -eq 0 ];
 		then
 			echo $(git rev-parse --show-toplevel)
+		else
+			echo ""
 	fi
-	echo ""
 }
 
 dir_me_up() {
 	local directory_name="${PWD##*/}"
 
-	echo "${directory_name:0:5}..."
+	if [ ${#directory_name} -gt 5 ];
+		then
+			echo "${directory_name:0:5}..."
+		else
+			echo "${directory_name:0}"
+	fi
 }
 
 branch_me_up() {
